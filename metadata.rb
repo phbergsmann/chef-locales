a1a77ea762958f1e6c25e9815acd548663fea681
Ed Bosher <edbosher@gmail.com>
2013-10-07 13:03:41 +0800

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "p.bergsmann@opendo.at"
 license          "All rights reserved"
 description      "Installs/Configures locales"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 supports "ubuntu"
 supports "debian"
