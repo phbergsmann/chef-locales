ff6c36cfe25edf582fbf0e07707349b2e32c30f5
pbergsmann <p.bergsmann@opendo.at>
2014-02-08 20:22:35 +0100

    [TASK] Raises versionnumber

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "p.bergsmann@opendo.at"
 license          "All rights reserved"
 description      "Installs/Configures locales"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.2.2"
 
 supports "ubuntu"
 supports "debian"
